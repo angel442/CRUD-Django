441a91f1fe262aa27a391b2a274b4ba8f33c0246
Angel Prat <aprat04@gmail.com>
2023-08-28 22:32:32 +0200

    setPorts and proxys and socket connection

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # This is a simple chat project that use sockets
 
-# This project use node with express.js, socket.io and React
+# This project use node with express.js, socket.io for the backend and Vite + React for the frontend
 
+# For run frontend
+ -- Go /frontend then
+ npm run dev
+ http://localhost:5173/
+
+
+# In case to run the server
+npm run dev
+http://localhost:3000
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import { http } from 'http';
+import http from 'http';
 import { Server as SocketServer } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
 
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+});
+
+io.on('connection', socket => {
+    console.log("client connected");
+})
 
 
 app.disable('x-powered-by');
@@ -15,6 +23,3 @@ server.listen(PORT, () => {
     console.log(`server listening on port http://localhost:${PORT}`);
 })
 
-app.use((req, res) => {
-    res.send('<h1>Server works</h1>');
-})
